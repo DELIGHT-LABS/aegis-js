9e165a7a8937201c1074ba513dde094271d236c8
Joowon Yun <joowon@delightlabs.io>
2024-05-04 02:42:50 +0900

    fix: citadel

--- src/citadel/citadel.test.ts
+++ src/citadel/citadel.test.ts
@@ -13,7 +13,7 @@ test("citadel2", async () => {
   const aegis = Aegis.dealShares(ProtocolVersion.V1, CipherVersion.V1, Algorithm.NoCryptAlgo, 3, 3, data, key);
 
   const token =
-    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.e30.JZ0DEwOTJDoSkcfe8t_FxPit4m12Cmy5SlGcdWj751pSY7idNWabDUZ0Q_DEKa051FupUEmIP4k4Iiuv6cq0Dg";
+    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1ODhiOTZmYy03ZGFkLTRmNmQtYjczNy1iZDE2YjNmMGZmNTgiLCJzc29fcHJvdmlkZXIiOiJHb29nbGUiLCJpYXQiOjE3MTY1ODc0NjZ9.BvBltHARZwW3gDKFEw5kO1gG_89ikLUJu4Xfx5zpRY2o9j405idWq4kygiC7Iqnga4Zs7VBJF7aBRg_d6gk3Bg";
   const URLs = [
     new URL("http://34.124.155.209:8080"),
     new URL("http://34.124.155.209:8081"),
@@ -31,4 +31,29 @@ test("citadel2", async () => {
   aegis2.payloads = res;
   const msg = aegis2.combineShares(key);
   expect(data).toEqual(msg);
+});
+
+test("citadel retrieve error", async () => {
+  // Test case 1
+  const key = new Uint8Array(Buffer.from("01234567890123456789012345678901"));
+  const data = new Uint8Array(Buffer.from("MESSAGE_1"));
+
+  const aegis = Aegis.dealShares(ProtocolVersion.V1, CipherVersion.V1, Algorithm.NoCryptAlgo, 3, 3, data, key);
+
+  const token =
+    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1ODhiOTZmYy03ZGFkLTRmNmQtYjczNy1iZDE2YjNmMGZmNTgiLCJzc29fcHJvdmlkZXIiOiJHb29nbGUiLCJpYXQiOjE3MTY1ODc0NjZ9.BvBltHARZwW3gDKFEw5kO1gG_89ikLUJu4Xfx5zpRY2o9j405idWq4kygiC7Iqnga4Zs7VBJF7aBRg_d6gk3Bg";
+  const URLs = [
+    new URL("http://34.124.155.209:8080"),
+    new URL("http://34.124.155.209:8081"),
+    new URL("http://34.124.155.209:8082"),
+  ];
+  const citadel = new Citadel(token, URLs);
+
+  const uuid = new Uint8Array(Buffer.from("588b96fc-7dad-4f6d-b737-bd16b3f0ff58"));
+  await citadel.store(aegis.payloads, uuid);
+
+  citadel.forts[0].url = new URL("http://34.124.155.209:808");
+
+  const res = await citadel.retrieve(uuid);
+  expect(res.length).toEqual(2);
 });
--- src/citadel/citadel.ts
+++ src/citadel/citadel.ts
@@ -6,9 +6,22 @@ interface Fort {
   url: URL;
 }
 
+interface PostSecretResponse {
+  id: string;
+}
+
+interface GetSecretResponse {
+  secret: string;
+}
+
+interface ErrorResponse {
+  error: string;
+  message: string;
+}
+
 class Citadel {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  private forts: Fort[] = [];
+  public forts: Fort[] = [];
 
   constructor(token: string, urls: URL[]) {
     this.forts = urls.map(url => ({ token, url }));
@@ -21,7 +34,7 @@ class Citadel {
 
     const strKey = Buffer.from(key).toString("base64");
 
-    const responses = [];
+    const responses: Promise<PostSecretResponse | ErrorResponse>[] = [];
     for (let i = 0; i < payloads.length; i++) {
       // encode to base64
       const data = Buffer.from(payloads[i]).toString("base64");
@@ -30,38 +43,53 @@ class Citadel {
       responses.push(res);
     }
 
-    await Promise.all(responses);
+    await Promise.all(responses).then(res => {
+      for (let i = 0; i < res.length; i++) {
+        const r = res[i] as ErrorResponse;
+        if (r.error) {
+          throw new Error(`${r.error}: ${r.message}`);
+        }
+      }
+    });
   }
 
   public async retrieve(key: Uint8Array): Promise<Payload[]> {
     const strKey = Buffer.from(key).toString("base64");
 
-    const responses: Promise<string>[] = [];
+    const responses: Promise<GetSecretResponse | ErrorResponse>[] = [];
     this.forts.map(async fort => {
       const res = this.getSecret(fort, strKey);
       responses.push(res);
     });
 
     const res: Payload[] = [];
-    await Promise.allSettled(responses)
-      .then(promisedRes => {
-        for (let i = 0; i < promisedRes.length; i++) {
-          if (promisedRes[i].status === "rejected") {
-            continue;
-          }
-
-          const pr = promisedRes[i] as PromiseFulfilledResult<string>;
-          res.push(Buffer.from(pr.value));
+    await Promise.allSettled(responses).then(promisedRes => {
+      for (let i = 0; i < promisedRes.length; i++) {
+        if (promisedRes[i].status === "rejected") {
+          continue;
         }
-      })
-      .catch(err => {
-        console.error(err);
-      });
+
+        const pr = promisedRes[i] as PromiseFulfilledResult<GetSecretResponse | ErrorResponse>;
+        // eslint-disable-next-line no-prototype-builtins
+        if (pr.value.hasOwnProperty("error")) {
+          const p = pr as PromiseFulfilledResult<ErrorResponse>;
+          console.error(`${p.value.error}: ${p.value.message}`);
+          continue;
+        }
+        const p = pr as PromiseFulfilledResult<GetSecretResponse>;
+        res.push(Buffer.from(p.value.secret, "base64"));
+      }
+    });
 
     return res;
   }
 
-  private async postSecret(fort: Fort, id: string, secret: string, overwrite: boolean = true) {
+  private async postSecret(
+    fort: Fort,
+    id: string,
+    secret: string,
+    overwrite: boolean = true,
+  ): Promise<PostSecretResponse | ErrorResponse> {
     const response = await fetch(`${fort.url.href}api/v0/secret`, {
       method: "POST",
       headers: {
@@ -80,24 +108,21 @@ class Citadel {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    // return await response.json();
-    return;
+    return response.json();
   }
 
-  private async getSecret(fort: Fort, id: string) {
-    const response = await fetch(`${fort.url.href}api/v0/secret/${id}`, {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  private async getSecret(fort: Fort, id: string): Promise<GetSecretResponse | ErrorResponse> {
+    const response = await fetch(`${fort.url.href}api/v0/secret`, {
       method: "GET",
       headers: {
         Accept: "application/text",
         Authorization: `Bearer ${fort.token}`,
       },
+      signal: AbortSignal.timeout(4000),
     });
 
-    if (!response.ok || response.status !== 200) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    return await response.text();
+    return response.json();
   }
 }
 
